d92fab755d97d952366ebd82bf862b3fd55a729b
jeongmin06 <jongmin06@naver.com>
2021-09-20 16:40:10 +0900

    Add Game.js import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Game from './game';
 
 ReactDOM.render(
   <React.StrictMode>
